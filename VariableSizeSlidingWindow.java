f8ef7e28f912a22358c4627cb8bc5eecb6f81e7b
Sachin Kumar <Sachinjnv100@outlook.com>
2026-05-08 22:01:33 +0530

    Update VariableSizeSlidingWindow.java

--- VariableSizeSlidingWindow.java
+++ VariableSizeSlidingWindow.java
@@ -1,6 +1,6 @@
 public class VariableSizeSlidingWindow{
     public static void main(String[] args) {
-        int[]arr={2,1,5,1,3};
+        int[]arr={2,1,5,1,,5,6,6,,6,5,9,9,3};
         int k=7;
         int left=0;
         int sum=0;
